3092272818522fcd539a49ad8c7c38546f08bad3
KrunalLathiya <krunal@appdividend.com>
2018-05-08 16:02:12 +0530

    VuexAxiosTutorial

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 export default {
   name: 'app',
@@ -15,14 +15,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
